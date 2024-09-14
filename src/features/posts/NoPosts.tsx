aab666e8e9110cba87ba1b305ca9bb1a8f9bfb28
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-14 18:37:12 +0200

    posts

--- src/features/posts/NoPosts.tsx
+++ src/features/posts/NoPosts.tsx
@@ -1,5 +1,5 @@
 export const NoPosts = () => {
-  return <div className="w-full mt-5 border border-gray-500 rounded-lg dark:border-gray-700 p-4">
+  return <div className="w-full mt-5 border border-gray-200 dark:border-gray-700 rounded-lg dark:border-gray-700 p-4">
       <h1 className="text-2xl font-bold">No posts yet</h1>
       <p className="text-gray-500 dark:text-gray-400">Be the first to post in a community</p>
     </div>
