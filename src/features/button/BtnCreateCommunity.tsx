887cb682cf2c777552196ef7a9ea296e4490568d
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-14 13:41:59 +0200

    community

--- src/features/button/BtnCreateCommunity.tsx
+++ src/features/button/BtnCreateCommunity.tsx
@@ -1,9 +1,12 @@
 "use client"
 import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
 
 export const BtnCreateCommunity = () => {
+  const router = useRouter()
+
   return <>
-    <Button className="w-full bg-orange-500 hover:bg-orange-700 dark:text-white">
+    <Button className="w-full bg-orange-500 hover:bg-orange-700 dark:text-white" onClick={() => router.push("/create-community")}>
         Create Community
     </Button>
   </>
