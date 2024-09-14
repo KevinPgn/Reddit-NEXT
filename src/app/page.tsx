94a934c3817a69d81c576d00f90f04ab3b2cbace
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-14 18:36:16 +0200

    posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import { PostForm } from "@/components/postForm/PostForm";
 import { SidebarHome } from "@/components/sidebarHome/SidebarHome";
+import { NoPosts } from "@/features/posts/NoPosts";
+import { Post } from "@/features/posts/Post";
+import { getPosts } from "@/features/posts/post.action";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getPosts()
+
   return (
   <div className="flex max-w-[1600px] mt-5 mx-auto gap-10">
     <div className="flex-1">
       <PostForm />
+      {posts.length === 0 ? (
+        <NoPosts />
+      ) : (
+        posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))
+      )}
     </div>
     <SidebarHome />
   </div>
---
+++ src/features/posts/Post.tsx
@@ -0,0 +1,5 @@
+export const Post = ({post}: {post: any}) => {
+  return <div className="w-full border border-gray-500 rounded-lg dark:border-gray-700 p-4">
+    
+  </div>
+}
