cc03975f94ef96cf60a2bc4cc257962ec5ac38a9
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-14 12:41:20 +0200

    mu community

--- src/features/sidebarLeft/MyCommunity.tsx
+++ src/features/sidebarLeft/MyCommunity.tsx
@@ -1,5 +1,10 @@
+import {ChevronDown, ChevronUp} from "lucide-react"
+
 export const MyCommunity = () => {
   return <>
-    
+    <div className="flex items-center cursor-pointer mt-7 justify-between">
+        <h2 className="text-sm text-gray-500 dark:text-gray-400 font-bold">COMMUNITY</h2>
+        <ChevronDown className="w-5 h-5 text-gray-500 dark:text-gray-400" />
+    </div>
   </>
 }
