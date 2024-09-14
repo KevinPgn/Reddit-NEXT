4a48ee610300c3366410f6898d29311d9b98b0d8
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-14 18:40:46 +0200

    posts

--- src/features/posts/NoPosts.tsx
+++ src/features/posts/NoPosts.tsx
@@ -1,6 +1,10 @@
+import { FileQuestion } from "lucide-react"
+
 export const NoPosts = () => {
-  return <div className="w-full mt-5 border border-gray-200 dark:border-gray-700 rounded-lg dark:border-gray-700 p-4">
-      <h1 className="text-2xl font-bold">No posts yet</h1>
-      <p className="text-gray-500 dark:text-gray-400">Be the first to post in a community</p>
+  return <div className="w-full mt-5 h-72 border border-gray-200 dark:border-gray-700 justify-center items-center flex flex-col rounded-lg p-4">
+    <div className="flex items-center w-fit p-5 bg-orange-500/20 rounded-full">
+      <FileQuestion className="w-10 h-10 text-orange-500" />
     </div>
+    <p className="text-xl font-bold mt-3">No post have been created</p>
+  </div>
 }
