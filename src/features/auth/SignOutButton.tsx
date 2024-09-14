0a4e5a6fc4c966c722b436e2a524e8e1b2dc8d90
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-14 13:16:54 +0200

    headers

--- src/features/auth/SignOutButton.tsx
+++ src/features/auth/SignOutButton.tsx
@@ -3,5 +3,5 @@ import { signOut } from "next-auth/react"
 import { Button } from "@/components/ui/button"
 
 export const SignOutButton = () => {
-  return <Button variant="destructive" onClick={() => signOut()}>Logout</Button>
+  return <Button className="w-full" variant="destructive" onClick={() => signOut()}>Logout</Button>
 }
--- src/features/headers/Authentication.tsx
+++ src/features/headers/Authentication.tsx
@@ -1,9 +1,30 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
 import {Menu} from "lucide-react"
+import {DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator} from "@/components/ui/dropdown-menu"
+import { SignOutButton } from "../auth/SignOutButton"
 
 export const Authentication = ({session}: {session: any}) => {
   return <div className="flex px-4 items-center gap-3 border border-gray-200 dark:border-zinc-800 rounded-full p-2">
-    <Menu className="w-5 h-5 cursor-pointer" />
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Menu className="w-5 h-5 cursor-pointer" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='mt-5'>
+      <DropdownMenuItem>
+          <span>Create Community</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <span>Create Post</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <span>Settings</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <SignOutButton />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
     <Avatar className="w-8 h-8">
       <AvatarImage src={session?.user?.image ?? ""}/>
     </Avatar>
