0dfad859d2df65e25a0d98d32c7e39a11537cc98
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-15 22:58:45 +0200

    post

--- src/components/createPost/CreatePostForm.tsx
+++ src/components/createPost/CreatePostForm.tsx
@@ -1,5 +1,16 @@
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
+import {Video, ListPlus} from "lucide-react"
+
 export const CreatePostForm = ({communityName}: {communityName: string}) => {
-  return <>
-    
-  </>
+  return <div className="w-full">
+    <p className="text-lg font-bold mb-2">Subreddit: <span className="font-normal text-orange-500 dark:text-orange-400">r/{communityName}</span></p>
+    <Tabs defaultValue="post" className="w-full">
+        <TabsList className="w-full">
+            <TabsTrigger value="post"><ListPlus className="w-4 h-4 mr-2" /> Post</TabsTrigger>
+            <TabsTrigger value="image"><Video className="w-4 h-4 mr-2" /> Image & Video</TabsTrigger>
+        </TabsList>
+        <TabsContent value="post"> </TabsContent>
+        <TabsContent value="image"> </TabsContent>
+    </Tabs>
+  </div>
 }
