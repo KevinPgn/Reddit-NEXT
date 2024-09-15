8686d456b52c3f7d04f73d36eb95df285fe4b4c2
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-15 23:19:43 +0200

    post

--- src/components/createPost/CreatePostForm.tsx
+++ src/components/createPost/CreatePostForm.tsx
@@ -11,8 +11,12 @@ export const CreatePostForm = ({communityName}: {communityName: string}) => {
             <TabsTrigger className="w-[50%] h-full" value="post"><ListPlus className="w-4 h-4 mr-2" /> Post</TabsTrigger>
             <TabsTrigger className="w-[50%] h-full" value="image"><Video className="w-4 h-4 mr-2" /> Image & Video</TabsTrigger>
         </TabsList>
-        <TabsContent value="post"><PostFormulaire /></TabsContent>
-        <TabsContent value="image"><ImageOrVideoFormulaire /></TabsContent>
+        <TabsContent value="post">
+            <PostFormulaire />
+        </TabsContent>
+        <TabsContent value="image">
+            <ImageOrVideoFormulaire />
+        </TabsContent>
     </Tabs>
   </div>
 }
