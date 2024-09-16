c5343e2be1b91bb06221780783942e7c5fd4578c
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-16 13:57:22 +0200

    community

---
+++ src/app/r/[communityName]/communityInfo.action.ts
@@ -0,0 +1,121 @@
+"use server"
+import prisma from "@/lib/prisma"
+import {z} from "zod"
+import { authenticatedAction } from "@/lib/safe-actions"
+/*
+// Reddit clone
+
+model Community {
+  id String @id @default(cuid())
+  name String @unique
+  description String?
+  imageUrl String?
+
+  creatorId String
+  creator User @relation(fields: [creatorId], references: [id])
+
+  posts Post[]
+  members Member[]
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+}
+
+model Member {
+  id String @id @default(cuid())
+  userId String
+  user User @relation(fields: [userId], references: [id])
+
+  communityId String
+  community Community @relation(fields: [communityId], references: [id])
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+
+  @@unique([userId, communityId])
+}
+
+model Post {
+  id String @id @default(cuid())
+  title String
+  content String
+  imageUrl String?
+  
+  communityId String
+  community Community @relation(fields: [communityId], references: [id])
+
+  authorId String
+  author User @relation(fields: [authorId], references: [id])
+
+  comments Comment[]
+  votes Vote[]
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+}
+
+model Comment {
+  id String @id @default(cuid())
+  content String
+
+  authorId String
+  author User @relation(fields: [authorId], references: [id])
+
+  postId String
+  post Post @relation(fields: [postId], references: [id])
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+}
+
+model Vote {
+  id String @id @default(cuid())
+  type String
+
+  postId String
+  post Post @relation(fields: [postId], references: [id])
+
+  authorId String
+  author User @relation(fields: [authorId], references: [id])
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+
+  @@unique([authorId, postId])
+}
+*/ 
+
+export const getCommunityInfo = async (communityName: string) => {
+    const community = await prisma.community.findUnique({
+        where: {
+            name: communityName
+        },
+        select: {
+            posts: {
+                select: {
+                    id: true,
+                    title: true,
+                    content: true,
+                    imageUrl: true,
+                    createdAt: true,
+                    updatedAt: true,
+                    author: {
+                        select: {
+                            id: true,
+                            name: true,
+                            image: true,
+                        }
+                    },
+                    _count: {
+                        select: {
+                            comments: true,
+                            votes: true
+                        }
+                    }
+                }
+            }
+        }
+    })
+    
+    return community
+}
--- src/app/r/[communityName]/page.tsx
+++ src/app/r/[communityName]/page.tsx
@@ -1,6 +1,7 @@
 import { PostForm } from '@/components/postForm/PostForm'
 import { SidebarCommunity } from '@/components/sidebarCommunity/SidebarCommunity'
 import React from 'react'
+import { getCommunityInfo } from './communityInfo.action'
 
 interface CommunityNamePageProps {
     params: {
@@ -8,13 +9,14 @@ interface CommunityNamePageProps {
     }
 }
 
-const CommunityNamePage = ({ params }: CommunityNamePageProps) => {
+const CommunityNamePage = async ({ params }: CommunityNamePageProps) => {
   const { communityName } = params
+  const community = await getCommunityInfo(communityName)
 
     return (
     <section className='flex mt-3 max-w-6xl mx-auto gap-8'>
         <div className='flex-1'>
-            <PostForm />
+            <PostForm community={community}/>
         </div>
 
         <SidebarCommunity communityName={communityName}/>
