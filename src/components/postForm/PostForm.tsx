4b3d598041475886df71f25702a3035e8d2e4871
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-17 01:27:29 +0200

    posts

--- src/components/postForm/PostForm.tsx
+++ src/components/postForm/PostForm.tsx
@@ -1,11 +1,17 @@
+"use client"
 import { Image, Link2 } from "lucide-react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
 
 export const PostForm = () => {
+  const router = useRouter()
+
   return <div className="w-[600px] max-lg:w-full border shadow-sm rounded-md p-3 flex items-center gap-4">
     <img src="/pfp.png" loading="lazy" className="w-10 h-10 rounded-full" alt="pfp reddit" />
-    <Input className="w-[400px] bg-transparent" placeholder="Create Post" />
+    <Input
+    onClick={() => router.push("/create-post")}
+    className="w-[400px] bg-transparent" placeholder="Create Post" />
     <Button variant="outline" className="px-3">
       <Image className="w-5 h-5" />
     </Button>
